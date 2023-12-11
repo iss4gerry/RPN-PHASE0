3c03af22b98d2cccc0203ab164f147ef17a2a10e
gerry <gerrydesrian48@gmail.com>
2023-12-11 22:05:28 +0700

    tugas part 6.5

---
+++ Part-6.5/soal5.js
@@ -0,0 +1,17 @@
+//5. Bintang asteriks
+//Problem buatlah bintang seperti berikut
+let input = 5;
+let bintang = ' ';
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+for(input;input >= 0;input--){
+
+    bintang += '*';
+    console.log(bintang);
+    
+}
