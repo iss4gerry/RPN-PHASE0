210a69d92ac76376dff6a60f2b982d2c934b23bf
gerry <gerrydesrian48@gmail.com>
2023-12-12 14:27:09 +0700

    tugas part 7.5

---
+++ Part-7.5/soal2.js
@@ -0,0 +1,19 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+let num1 = 1;
+let num2 = 2;
+
+function calculateMultiply(n1, n2){
+    return n1 * n2;
+}
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+
+
+// ------------------------------------
+console.log(hasilPerkalian);
+// -----------------------------------
