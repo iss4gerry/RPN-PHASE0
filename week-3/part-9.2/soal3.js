a6b6747849be1b87d1ee8551c5c0178c5c2dfaa4
gerry <gerrydesrian48@gmail.com>
2023-12-13 15:07:25 +0700

    tugas part 9.2 revisi

--- week-3/part-9.2/soal3.js
+++ week-3/part-9.2/soal3.js
@@ -1,8 +1,8 @@
 function cariMedian(arr) {
     let angkaUrut = [...arr].sort((a, b) => a - b);
     let index;
-    for(let i = 0;i < arr.length;i++){
-        if(arr.length % 2 !== 0){
+
+    if(arr.length % 2 !== 0){
             index = (arr.length + 1)/2;
             return angkaUrut[index - 1];
         }else{
@@ -10,11 +10,8 @@ function cariMedian(arr) {
             let nilai2 = arr.length / 2;
             return (angkaUrut[nilai1] + angkaUrut[nilai2]) / 2;
         }
-        
-
     }
-  }
-  
+
   // TEST CASES
   console.log(cariMedian([1, 2, 3, 4, 5])); // 3
   console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
